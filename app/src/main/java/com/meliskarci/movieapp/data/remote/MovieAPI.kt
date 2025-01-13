9b3cc5939fbf88a8517816bed5e0bfae7a3a847e
Melis Karcı <129781468+meliskarci@users.noreply.github.com>
2025-01-14 00:55:02 +0300

    Update MovieAPI.kt

--- app/src/main/java/com/meliskarci/movieapp/data/remote/MovieAPI.kt
+++ app/src/main/java/com/meliskarci/movieapp/data/remote/MovieAPI.kt
@@ -6,10 +6,6 @@ import retrofit2.http.Query
 
 interface MovieAPI {
 
-    //https://www.omdbapi.com/?apikey=f2871f7&s=batman
-    //https://www.omdbapi.com/?apikey=f2871f7&i=tt0372784
-    //OMDb API: http://www.omdbapi.com/?i=tt3896198&apikey=f2871f7
-
     @GET(".")
     suspend fun getMovies(
         @Query("s") searchString : String,
