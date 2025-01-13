5f24c170b5ba6416c8e6e9a69cdb09d1250b4e68
Melis Karcı <129781468+meliskarci@users.noreply.github.com>
2025-01-14 00:53:13 +0300

    Update Constants.kt

--- app/src/main/java/com/meliskarci/movieapp/util/Constants.kt
+++ app/src/main/java/com/meliskarci/movieapp/util/Constants.kt
@@ -1,7 +1,4 @@
 package com.meliskarci.movieapp.util
 
 object Constants {
-    const val API_KEY = "f2871f7"
-    const val BASE_URL = "http://www.omdbapi.com"
-    const val IMDB_ID = "imdb_id"
 }
